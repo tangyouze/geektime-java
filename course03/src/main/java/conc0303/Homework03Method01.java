c79a52abd54f226db7027fc4004ed018e84a9942
tyz <tyz@1token.trade>
2022-03-27 12:32:53 +0800

    first init copy code

---
+++ course03/src/main/java/conc0303/Homework03Method01.java
@@ -0,0 +1,57 @@
+package conc0303;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
+ * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
+ * 写出你的方法，越多越好，提交到github。
+ * <p>
+ * 一个简单的代码参考：
+ *
+ * @author tyz
+ */
+public class Homework03Method01 {
+
+    static int result = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        long start = System.currentTimeMillis();
+
+        // 在这里创建一个线程或线程池，
+        // 异步执行 下面方法
+
+        Thread thread = new Thread(() -> {
+            Homework03Method01.result = sum();
+        });
+        thread.start();
+
+        while (true) {
+            if (Homework03Method01.result != 0) {
+                break;
+            }
+            System.out.println("等待结果...");
+            Thread.sleep(10);
+        }
+
+        // 这是得到的返回值
+
+        // 确保  拿到result 并输出
+        System.out.println("异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+
+        // 然后退出main线程
+    }
+
+    private static int sum() {
+        return fibo(42);
+    }
+
+    private static int fibo(int a) {
+        if (a < 2)
+            return 1;
+        return fibo(a - 1) + fibo(a - 2);
+    }
+}
