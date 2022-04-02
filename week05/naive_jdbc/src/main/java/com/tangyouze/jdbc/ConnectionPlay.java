e4ccf51941d09c5f2c48227a8f59d515401e026a
tyz <tyz@1token.trade>
2022-04-02 21:46:06 +0800

    10.（必做）研究一下 JDBC 接口和数据库连接池，掌握它们的设计和用法： 1）使用 JDBC 原生接口，实现数据库的增删改查操作。

---
+++ week05/naive_jdbc/src/main/java/com/tangyouze/jdbc/ConnectionPlay.java
@@ -0,0 +1,92 @@
+package com.tangyouze.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author tyz
+ */
+public class ConnectionPlay {
+    private Connection conn;
+    private final String URL = "jdbc:mysql://localhost:3306/mysql";
+    private final String NAME = "root";
+    private final String PASSWORD = "";
+
+    void run() throws Exception {
+//        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //2.获得数据库的连接
+        conn = DriverManager.getConnection(URL, NAME, PASSWORD);
+
+        try {
+            createTable();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            insert();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            select();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            dropTable();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private String randomString() {
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        int length = str.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            int number = (int) (Math.random() * length);
+            sb.append(str.charAt(number));
+        }
+        return sb.toString();
+
+    }
+
+    private void insert() throws Exception {
+        Statement stmt = conn.createStatement();
+        for (int i = 0; i < 10; i++) {
+            // generate random string
+            boolean execute = stmt.execute("insert into play1(name) values ('" + randomString() + "')");
+            System.out.println(execute);
+        }
+
+    }
+
+    private void select() throws Exception {
+        Statement stmt = conn.createStatement();
+        ResultSet resultSet = stmt.executeQuery("select * from play1");
+        System.out.println(resultSet);
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2));
+        }
+
+    }
+
+    void createTable() throws Exception {
+        Statement stmt = conn.createStatement();
+        boolean execute = stmt.execute("create table play1(id int auto_increment primary key ,name varchar(20))");
+        System.out.println(execute);
+    }
+
+    void dropTable() throws Exception {
+        Statement stmt = conn.createStatement();
+        boolean execute = stmt.execute("drop table play1");
+        System.out.println(execute);
+    }
+}
---
+++ week05/naive_jdbc/src/main/java/com/tangyouze/jdbc/Entry.java
@@ -0,0 +1,18 @@
+package com.tangyouze.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author tyz
+ */
+public class Entry {
+
+    public static void main(String[] args) throws Exception {
+
+        ConnectionPlay play = new ConnectionPlay();
+        play.run();
+    }
+}
