7b68c8d267d48729f4114a90260323307a438b40
tyz <tyz@1token.trade>
2022-04-03 20:05:18 +0800

    8.（必做）给前面课程提供的 Student/Klass/School 实现自动配置和 Starter。

---
+++ week05/sprintboot01/src/main/java/com/example/sprintboot01/configure/WebConfiguration.java
@@ -0,0 +1,2 @@
+package com.example.sprintboot01.configure;
+
--- week05/sprintboot01/src/main/java/com/example/sprintboot01/controller/Entry.java
+++ week05/sprintboot01/src/main/java/com/example/sprintboot01/controller/Entry.java
@@ -1,13 +1,21 @@
 package com.example.sprintboot01.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Entry {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("/")
     public String index() {
-        return "Hello World";
+        stringRedisTemplate.opsForValue().increment("key");
+        String value = stringRedisTemplate.opsForValue().get("key");
+        return value;
     }
 
 }
