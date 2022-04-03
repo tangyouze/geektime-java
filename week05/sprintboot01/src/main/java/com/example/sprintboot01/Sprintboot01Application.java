d075e22b04ec4f87b0e606503c3e8782acebb5a7
tyz <tyz@1token.trade>
2022-04-03 19:51:09 +0800

    sprint boot init

---
+++ week05/sprintboot01/src/main/java/com/example/sprintboot01/Sprintboot01Application.java
@@ -0,0 +1,14 @@
+package com.example.sprintboot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprintboot01Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(Sprintboot01Application.class, args);
+    }
+
+}
---
+++ week05/sprintboot01/src/test/java/com/example/sprintboot01/Sprintboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprintboot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
